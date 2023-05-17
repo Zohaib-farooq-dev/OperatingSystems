e37b8456597cbb70654b1831a9f9d4f0e78461c1
Zohaib-farooq-dev <zohaibx595@gmail.com>
2023-05-17 11:06:56 +0500

    Shell Scripts in C language

---
+++ ShellScripts/Shell2.sh
@@ -0,0 +1,20 @@
+clear 
+echo "Enter your name"
+read name
+echo $name
+
+read -p "Enter your name : "  name
+echo $name
+echo "Entered Name : " $name
+clear
+
+read -p "Enter your name : "  name
+read -p "Enter your id : "  ID
+echo "Entered Name : " $name "and the entered id is :" $ID
+
+echo "Entered name is : $name and the entered id is : "$ID
+clear
+
+name=123
+echo $name
+clear
---
+++ ShellScripts/VariableOperationsShell.sh
@@ -0,0 +1,11 @@
+clear
+num1=12
+num2=67
+num3=$((num1+num2))
+
+echo $num3
+
+read -p "Enter the number 1 :" num4
+read -p "Enter the number 2: " num5
+
+echo $((num4+num5))
